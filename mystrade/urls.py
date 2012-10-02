99dc57908ff42658e963816978d59846f83d5b76
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2012-10-02 14:00:59 +0200

    implemented logout

--- mystrade/urls.py
+++ mystrade/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
-from django.core.urlresolvers import reverse_lazy
 from django.views.generic.base import TemplateView
 
 # Uncomment the next two lines to enable the admin:
@@ -12,7 +11,7 @@ urlpatterns = patterns('',
         name='login'),
     url(r'^welcome/$', login_required(TemplateView.as_view(template_name = 'userprofile/welcome.html')), 
         name = 'welcome'),
-    url(r'^logout$', 'django.contrib.auth.views.logout',
+    url(r'^logout$', 'django.contrib.auth.views.logout_then_login',
         name='logout'),
     url(r'^profile/', include('userprofile.urls')),
     
