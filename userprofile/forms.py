f836848955f24a49368b4c1680ecdea2afd51e2a
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2012-10-08 18:15:18 +0200

    fix: only one non empty password field must not validate

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -8,21 +8,21 @@ class UserForm(forms.ModelForm):
     new_password2 = forms.CharField(label="New password confirmation", required = False,
                                     widget=forms.PasswordInput)
 
-    class Meta:
-        model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
-        
     def clean_new_password2(self):
         password1 = self.cleaned_data.get('new_password1')
         password2 = self.cleaned_data.get('new_password2')
         if password1 and password2:
             if password1 != password2:
                 raise forms.ValidationError("The two password fields didn't match.")
+        elif (not password1 and password2) or (password1 and not password2):
+            raise forms.ValidationError("The two password fields didn't match.")
         return password2
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['bio', 'contact']
-
-#SetPasswordForm 
