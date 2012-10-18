78869b5ae5cd476cd4131680e1de1897e452c3d8
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2012-10-18 18:29:47 +0200

    rulecard display in css table

--- scoring/forms.py
+++ scoring/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class RuleCardFormParse(forms.Form):
     card_id = forms.CharField(widget = forms.HiddenInput())
-    selected_rule = forms.BooleanField(required = False)
+    selected_rule = forms.BooleanField(required = False, label = "Keep")
 
 class RuleCardFormDisplay(RuleCardFormParse):
     public_name = forms.CharField()
