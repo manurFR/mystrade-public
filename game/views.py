af40c30279a78ee338304865a28456fa32af42a1
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2012-11-07 14:57:55 -0800

    'create new game' button

--- game/views.py
+++ game/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponse
+
+@permission_required('game.add_game')
+def create_game(request):
+    return HttpResponse("Let's create a new game...")
--- mystrade/urls.py
+++ mystrade/urls.py
@@ -7,14 +7,12 @@ from django.views.generic.base import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'django.contrib.auth.views.login', {'template_name': 'userprofile/login.html'}, 
-        name='login'),
-    url(r'^welcome/$', login_required(TemplateView.as_view(template_name = 'userprofile/welcome.html')), 
-        name = 'welcome'),
-    url(r'^logout$', 'django.contrib.auth.views.logout_then_login',
-        name='logout'),
+    url(r'^$', 'django.contrib.auth.views.login', {'template_name': 'userprofile/login.html'}, name='login'),
+    url(r'^welcome/$', login_required(TemplateView.as_view(template_name = 'welcome.html')), name = 'welcome'),
+    url(r'^logout$', 'django.contrib.auth.views.logout_then_login', name='logout'),
     url(r'^profile/', include('userprofile.urls')),
     url(r'^scoring/', include('scoring.urls')),
+    url(r'^game/',    include('game.urls')),
     
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^caramba/doc/', include('django.contrib.admindocs.urls')),
