703bfb308024c4d4fe7f8daeccf7a94b84fb041a
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2012-11-08 11:35:13 +0100

    create game: view test for permission

--- game/tests.py
+++ game/tests.py
@@ -1,16 +1,19 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from django.contrib.auth.models import User, Permission
 from django.test import TestCase
 
+class ViewsTest(TestCase):
+    def setUp(self):
+        self.testUserCanCreate = User.objects.create_user('testCanCreate', 'test@aaa.com', 'test')
+        self.testUserCanCreate.user_permissions.add(Permission.objects.get(codename = 'add_game'))
+        self.testUserNoCreate  = User.objects.create_user('testNoCreate', 'test@aaa.com', 'test')
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_create_game_only_with_the_permission(self):
+        self.client.login(username = 'testCanCreate', password = 'test')
+        response = self.client.post("/game/create/")
+        self.assertEqual(200, response.status_code)
+        self.client.logout()
+
+        self.client.login(username = 'testNoCreate', password = 'test')
+        response = self.client.post("/game/create/")
+        self.assertEqual(302, response.status_code)
+        self.client.logout()
