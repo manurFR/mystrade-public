9506cc426bc304eeef38f021bdbbb973e862570d
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2013-01-20 14:54:41 -0800

    trade proposal: add trade comment

--- game/models.py
+++ game/models.py
@@ -49,6 +49,8 @@ class Trade(models.Model):
     rules = models.ManyToManyField(RuleInHand)
     commodities = models.ManyToManyField(CommodityInHand, through='TradedCommodities')
 
+    comment = models.TextField()
+
     status = models.CharField(max_length = 15, default = "INITIATED") # INITIATED, CANCELLED, ACCEPTED or DECLINED
     creation_date = models.DateTimeField(default = now())
     closing_date = models.DateTimeField(null = True)
