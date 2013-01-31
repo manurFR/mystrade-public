b34a71f4fb2ab84b48d2e92d6cc7838bcb701008
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2013-01-31 11:49:00 +0100

    tests: split tests by app in runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -3,4 +3,19 @@
 
 APPS=`python -c "from mystrade.settings import MYSTRADE_APPS; print ' '.join(MYSTRADE_APPS)"`
 
-./manage.py test ${APPS}
+for app in ${APPS}; do
+	echo "**** Tests for app:           ${app}"
+	python -Wall manage.py test ${app}
+  if [[ $? != 0 ]]; then
+    echo "TESTS FAILURE !"
+    exit 1
+  fi
+  echo "/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\"
+done
+
+echo
+echo "=================="
+echo "  All tests OK    "
+echo "=================="
+
+exit 0
