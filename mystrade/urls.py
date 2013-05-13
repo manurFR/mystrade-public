9a0196ce04e4b2e8d171900504d8b5d0fd0c2c30
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2013-05-13 18:30:33 +0200

    rules: first words of the rules page (in French)

--- mystrade/urls.py
+++ mystrade/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView, RedirectView
 
 # Uncomment the next two lines to enable the admin:
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$',         'django.contrib.auth.views.login', {'template_name': 'userprofile/login.html'}, name='login'),
-    url(r'^welcome/$', 'django.views.generic.simple.redirect_to', {'url': '/game/'}),
+    url(r'^welcome/$', RedirectView.as_view(url = '/game/')),
     url(r'^logout$',   'django.contrib.auth.views.logout_then_login', name='logout'),
+    url(r'^rules/',    TemplateView.as_view(template_name = 'rules.html'), name='rules'),
     url(r'^profile/',  include('userprofile.urls')),
     url(r'^game/',     include('game.urls')),
     url(r'^trade/',    include('trade.urls')),
