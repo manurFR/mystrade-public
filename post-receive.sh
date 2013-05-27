8bfcefdf2d1978dcb7dac99f31d38c220c97ebe3
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2013-05-27 15:54:49 +0200

    deployment preparation: project in the home directory

--- post-receive.sh
+++ post-receive.sh
@@ -2,12 +2,12 @@
 # -*- coding: UTF8 -*-
 
 # Launched by a git hook :
-#  $ echo "#!/bin/sh\nsh /home/mystrade/public/post-receive.sh" > /home/mystrade/git/mystrade.git/hooks/post-receive
+#  $ echo "#!/bin/sh\nsh /home/mystrade/post-receive.sh" > /home/mystrade/git/mystrade.git/hooks/post-receive
 #  $ chmod +x /home/mystrade/git/mystrade.git/hooks/post-receive
 
 # Remember that this script will be executed by the unix user who push to the
 # git repository ; and the script will be executed in ~/public.
-export GIT_WORK_TREE=/home/mystrade/public
+export GIT_WORK_TREE=/home/mystrade
 export GIT_DIR=/home/mystrade/git/mystrade.git
 git checkout -f
 git reset --hard
