01656433496e1d49d3ec17728701614b86850d36
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2013-05-31 15:03:14 +0200

    deployment preparation: upgrade bleach to 1.2.2 to fix a bug (https://github.com/jsocol/bleach/issues/94)

--- production/post-deploy.sh
+++ production/post-deploy.sh
@@ -5,6 +5,8 @@
 PROD_DIR=$HOME/mystrade/production
 DEPLOY_WITH_WARNINGS=0
 
+PIP_BUILD_DIRECTORY=/tmp/pip-build-mystrade
+
 # Update .bash_profile
 cp -f $PROD_DIR/.bash_profile $HOME
 if (( $? )); then
@@ -14,6 +16,14 @@ fi
 
 # Source .bash_profile (to get PYTHONPATH, DJANGO_SETTINGS_MODULE, etc. which make manage.py work)
 source $HOME/.bash_profile
+
+# A bug in pip sometimes changes the version of a requirement when a former version was already downloaded
+#  ("Requested <mylib>==1.2.2, but installing version 1.2.1"). Workaround: we clean the build directory/cache.
+rm -rf $PIP_BUILD_DIRECTORY/*
+if (( $? )); then
+    DEPLOY_WITH_WARNINGS=1
+    echo "** WARNING ** [rm -rf $PIP_BUILD_DIRECTORY/*] failed";
+fi
 
 # Update requirements, installing the modules in personal modules directory (python-modules) per alwaysdata policy
 # note: the first time we needed PYTHONPATH=$HOME/python-modules easy_install-2.6 --install-dir $HOME/python-modules markdown==2.3.1
