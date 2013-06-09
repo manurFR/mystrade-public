cc5373ed7bbf18db2cfcb8c5cc056dd8e85182e1
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2013-06-09 05:55:00 -0700

    stats: added model

--- utils/models.py
+++ utils/models.py
@@ -1,1 +1,16 @@
-''' dummy required by django '''
+from django.db import models
+from django.utils.timezone import now
+from game.models import Game
+from mystrade import settings
+from trade.models import Trade
+
+class StatsScore(models.Model):
+    game = models.ForeignKey(Game)
+    player = models.ForeignKey(settings.AUTH_USER_MODEL)
+    trade = models.ForeignKey(Trade, null = True) # after which completed trade this stat was taken, if not null
+
+    dateScore = models.DateTimeField(default = now)
+
+    score = models.IntegerField()
+
+    random = models.BooleanField(default = False)
