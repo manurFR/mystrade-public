a359c938d4ceb65a5e4c8a9c2fc719680fbe4c54
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2013-09-23 11:50:32 +0200

    initial data: change shade of pink cards for RMX in order to have contrast with the stripes

---
+++ ruleset/migrations/0002_update_initial_data.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-                                                                    my_fixture
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        "Write your forwards methods here."
+        # Note: Remember to use orm['appname.ModelName'] rather than "from appname.models..."
+        from django.core.management import call_command
+        call_command("loaddata", "initial_data.json")
+
+    def backwards(self, orm):
+        "Write your backwards methods here."
+
+    models = {
+        u'ruleset.commodity': {
+            'Meta': {'object_name': 'Commodity'},
+            'color': ('django.db.models.fields.CharField', [], {'default': "'white'", 'max_length': '20'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'ruleset': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ruleset.Ruleset']"}),
+            'value': ('django.db.models.fields.IntegerField', [], {'null': 'True'})
+        },
+        u'ruleset.rulecard': {
+            'Meta': {'object_name': 'RuleCard'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'glob': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'db_column': "'global'"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mandatory': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'public_name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'ref_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'}),
+            'ruleset': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ruleset.Ruleset']"}),
+            'step': ('django.db.models.fields.IntegerField', [], {'null': 'True'})
+        },
+        u'ruleset.ruleset': {
+            'Meta': {'object_name': 'Ruleset'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '510'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'module': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['ruleset']
+    symmetrical = True
