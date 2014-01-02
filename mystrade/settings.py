e26a5cd5ec4761fc3a15b1dd51d320453eb6a4f0
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2014-01-02 16:03:28 +0100

    settings: remove unused 'profile' directory from static_dirs

--- mystrade/settings.py
+++ mystrade/settings.py
@@ -101,7 +101,6 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(SITE_ROOT, 'static'),
-    os.path.join(SITE_ROOT, 'static/profile'),
 )
 
 # List of finder classes that know how to find static files in various locations.
